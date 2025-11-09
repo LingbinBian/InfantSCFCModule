d4aa85487c834477a8c4ffb668d0da2a5a350ffa
Lingbin Bian <51219350+LingbinBian@users.noreply.github.com>
2025-11-09 12:11:33 +0800

    Delete Script_control directory

--- Script_control/dirichlet_pdf.m
+++
@@ -1,7 +0,0 @@
-function p = dirichlet_pdf(X, alpha)
-    % X is an N-by-K matrix where each row is a point in the K-dimensional simplex
-    % alpha is a 1-by-K vector of Dirichlet parameters
-    gamma_prod = prod(gamma(alpha));
-    gamma_sum = gamma(sum(alpha));
-    p = gamma_sum / gamma_prod * prod(X.^(alpha - 1), 2);
-end
